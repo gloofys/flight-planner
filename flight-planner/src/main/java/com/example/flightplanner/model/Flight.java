d8291faddaaffad0cd1e06c1996bea400a572e16
fredb <fred.brosman06@gmail.com>
2025-03-16 13:14:50 +0200

    feat: Added Flight entity and PostgreSQL configuration
    - Implemented Flight entity
      - Fields: id, departure, destination, flightTime, duration, price, layovers, airline
    
    - Configured PostgreSQL database
      - compose.yaml for PostgreSQL setup
      - database on port 5433

---
+++ flight-planner/src/main/java/com/example/flightplanner/model/Flight.java
@@ -0,0 +1,33 @@
+package com.example.flightplanner.model;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "flights")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String departure;
+
+    private String destination;
+
+    @Column(name = "flight_time")
+    private LocalDateTime flightTime;
+
+    private int duration;
+
+    private double price;
+
+    private int layovers;
+
+    private String airline;
+}
