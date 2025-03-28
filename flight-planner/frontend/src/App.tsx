7b9195d05ebb6ef8b115e3e9b2c44d2f2f972c3b
fredb <fred.brosman06@gmail.com>
2025-03-28 11:18:01 +0200

    App.tsx added a new route to the seat selection page.

--- flight-planner/frontend/src/App.tsx
+++ flight-planner/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FlightList from "./components/FlightList.tsx";
 import FlightDetails from "./pages/FlightDetails.tsx";
 import { FlightFiltersProvider } from "./context/FlightFiltersContext.tsx";
+import SeatSelection from "./pages/SeatSelection.tsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
             <Route path="/" element={<FlightList />} />
             <Route path="/flights/:id" element={<FlightDetails />} />
+              <Route path="/flights/:id/seats" element={<SeatSelection />} />
           </Routes>
         </FlightFiltersProvider>
       </Router>
