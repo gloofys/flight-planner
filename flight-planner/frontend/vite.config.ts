8262b26ae5cf82cacc32912ccb9780f53ad861f6
fredb <fred.brosman06@gmail.com>
2025-03-30 17:35:31 +0300

    added CorsConfig.java to get docker working.
    Added plane icon to index.html

--- flight-planner/frontend/vite.config.ts
+++ flight-planner/frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
@@ -14,14 +15,12 @@ export default defineConfig({
     build: {
         rollupOptions: {
             output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('react')) return 'vendor-react'
-                        if (id.includes('@mui')) return 'vendor-mui'
-                        return 'vendor-other'
-                    }
+                manualChunks: {
+                    react: ['react', 'react-dom'],
+                    mui: ['@mui/material', '@mui/icons-material'],
+                    baseui: ['@base-ui-components/react'],
                 },
             },
         },
     },
-})
+});
